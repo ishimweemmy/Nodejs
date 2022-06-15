7e57905fe44719d6014a6af6181a0f0654518e57
ishimwe-emma <ishimweemmanuel2005@gmail.com>
2022-06-15 21:58:19 +0200

    started the mvp for taskmanager

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,10 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+
+app.get('/hello', (req, res)=>{
+    res.send('hello there')
+})
+
+app.listen(port, ()=>console.log(`server is up and running on port ${port} ...`))
