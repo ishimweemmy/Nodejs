08ea08918464a13c811e70cbe59301a2c503f97f
ishimwe-emma <ishimweemmanuel2005@gmail.com>
2022-06-17 21:35:54 +0200

    made the changes to the task controllers

--- 03-task-manager/final/controllers/tasks.js
+++ 03-task-manager/final/controllers/tasks.js
@@ -20,6 +20,7 @@ const getTask = asyncWrapper(async (req, res, next) => {
 
   res.status(200).json({ task })
 })
+
 const deleteTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params
   const task = await Task.findOneAndDelete({ _id: taskID })
